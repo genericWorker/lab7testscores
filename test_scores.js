618eda1ead7247e9d17f80b4078df14da29cd024
genericWorker <57064040+genericWorker@users.noreply.github.com>
2021-10-14 21:06:04 -0400

    Add files via upload

--- test_scores.js
+++ test_scores.js
@@ -1,86 +1,124 @@
-let names = ["Ben", "Joel", "Judy", "Anne"];
-let scores = [88, 98, 77, 88];
 
-var $ = function (id) { return document.getElementById(id); };
+let namesArr = ['Ben', 'Joel', 'Judy', 'Anne'];
+let scoresArr = [88, 98, 77, 88];
 
-let avgScores = scores.reduce((a, b) => a + b) / scores.length;
-
-function getHighScore() {
-	let i=0,max=0,len = scores.length;
-	let name = '';
-	for (i; i < len; i++) {
-		if (scores[i] > max) {
-			max = scores[i];
-			name = names[i];
-		}
-	}
-	return name + ' with score of ' + max;
+function getAvgScore() {
+    let i = 0,
+        sum = 0,
+        len = scoresArr.length;
+    let name = '';
+    for (i; i < len; i++) {
+        sum += scoresArr[i];
+    }
+    return sum / len;
 }
 
-function displayResults(avgScore, highScoreName, highScore) {
-	let results = $('results');
-	if (results.style.display == 'inline') {
-		results.syle.display = 'none';
-	}
-	else {
-		results.style.display = 'inline';
-	}
-	$('avgScore').innerHTML = avgScores;
-	$('highScore').innerHTML = getHighScore();
+function getHighScore() {
+    let i = 0,
+        max = 0,
+        len = scoresArr.length;
+    let name = '';
+    for (i; i < len; i++) {
+        if (scoresArr[i] > max) {
+            max = scoresArr[i];
+            name = namesArr[i];
+        }
+    }
+    return name + ' with score of ' + max;
+}
+
+function initializeResults() {
+    let results = $('#results');
+    let high = getHighScore();
+    let avg = getAvgScore().toFixed(1);
+    $('#highScore').html(high);
+    $('#avgScore').html(avg);
+}
+
+
+function displayResults() {
+    let results = $('#results');
+    results.toggle();
 }
 
 //  insert a new element in the html table after current index.  Assumes, names and scores are updates already.
-function insertTableElement(scoresTable, rowIndex) {
-	    var rowCount = scoresTable.rows.length;
-		let row = scoresTable.insertRow( rowCount );
-		let name = row.insertCell( 0 );
-		var score = row.insertCell( 1 );
-		name.innerHTML = names[rowCount-1];
-		score.innerHTML = scores[rowCount-1];
+function insertTableElement(scoresTable, index) {
+    $('scoresTable tr:last').after('<tr> <td>' + namesArr[index] + '</td><td>' + scoresArr[index] + '</td></tr>');
 }
 
-// display the scores table if it's not already displayed.  In this case add the table elements to the
-// table
+function insertNewTableElement(newName, newScore) {
+    let rowCount = document.getElementById("scores_table").getElementsByTagName("tr").length;
+    $('#scoresTable tr:last').append('<tr> <td>' + newName + '</td><td>' + newScore + '</td></tr>');
+    rowCount = document.getElementById("scores_table").getElementsByTagName("tr").length;
+}
+
+function initializeScoresTable() {
+    // Remove header row from table
+    $('#scores_table tr').slice(1).remove();
+    for (let i = 0; i < scoresArr.length; i++) {
+        insertNewTableElement($('#scores_table tr:last').after('<tr> <td>' + namesArr[i] + '</td><td>' + scoresArr[i] + '</td></tr>'));
+    }
+}
+
 function displayScores() {
-    if ($("scores").style.display == 'none') {
-		$( "scores" ).style.display = "inline";
-		for (let i = 0; i < scores.length; i++) {
-			insertTableElement( $( "scores_table" ), i );
-		}
-	}
+    let scores = $('#scores');
+    scores.toggle();
 }
 
 function addScore() {
-	if ($('score').value == '' || $('name').value == '') {
-		alert("Name and score must have values");
-		return;
-	}
-	displayScores();
-	scores.push(parseInt($("score").value));
-	names.push($("name").value);
-	insertTableElement( $( "scores_table" ), scores.length);
-	$("score").value = "";
-	$("name").value = "";
-	displayResults();
+    let score = $('#score');
+    let name = $('#name');
+    if (score.val() === '' || name.val() === '') {
+        alert('Name and score must have values');
+        return;
+    }
+    scoresArr.push(parseInt(score.val()));
+    namesArr.push($("#name").val());
+    initializeScoresTable();
+    insertNewTableElement( name.val(), score.val() );
+    score.val('');
+    name.val('');
+    initializeResults();
+    $('#scores').show();
+    $('#results').show();
 }
 
 window.onload = function () {
-	$("add").onclick = addScore;
-	$("display_results").onclick = displayResults;
-	$("display_scores").onclick = displayScores;
-	var name = $("name");
-	var score = $("score");
+    $('#display_results').on('click',  function() {
+        displayResults();
+    });
+     
+    $('#display_scores').on('click',  function() {
+        displayScores();
+    });
+    $('#add').on('click',  function() {
+        addScore();
+    });
 
-	name.addEventListener("keyup", function(event) {
-		if (event.key == "Enter") {
-			$("score").focus();
-		}
-	});
-	score.addEventListener("keyup", function(event) {
-		if (event.key == "Enter") {
-			$("add").click();
-		}
-	});
-};
+    let name = $('#name');
+    let score = $('#score');
 
+    name.focus();
+    initializeResults();
+    initializeScoresTable();
 
+    // register jQuery extension
+    // used for changing focus on enter ekey
+    jQuery.extend(jQuery.expr[':'], {
+        focusable: function(el, index, selector) {
+            return $(el).is('a, button, :input, [tabindex]');
+        }
+    });
+
+    //  Changes focus to next input on enter key
+    $(document).on('keypress', 'input,select', function(e) {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            // Get all focusable elements on the page
+            let $canfocus = $(':focusable');
+            let index = $canfocus.index(this) + 1;
+            if (index >= $canfocus.length) index = 0;
+            $canfocus.eq(index).focus();
+        }
+    });
+}
